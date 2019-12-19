36f6d0cae24235f4dc0bd39d9a15d350bcc09933
Sellec <echelonww@gmail.com>
2019-12-19 21:35:41 +0300

    1

--- Projects/Core/Core.DB/dbo/Tables/dbo.CurrencyCoefficient.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[CurrencyCoefficient] (
-    [IdCurrencyFrom]   INT              NOT NULL,
-    [IdCurrencyTo]     INT              NOT NULL,
-    [PriceCoefficient] DECIMAL (30, 15) NOT NULL,
-    [DateChange]       INT              NOT NULL,
-    CONSTRAINT [PK_currencycoefficient_IdCurrencyFrom] PRIMARY KEY CLUSTERED ([IdCurrencyFrom] ASC, [IdCurrencyTo] ASC)
-);
-
---
+++ Projects/Core/Core.DB/dbo/User Defined Types/dbo.TVP_ItemLink.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TVP_ItemLink] AS TABLE (
+    [ItemIdType] INT              DEFAULT ((0)) NOT NULL,
+    [ItemId]     INT              DEFAULT ((0)) NOT NULL,
+    [ItemKey]    NVARCHAR (200)   DEFAULT ('') NOT NULL,
+    [LinkId]     UNIQUEIDENTIFIER DEFAULT (NULL) NOT NULL,
+    [IdUser]     INT              NULL,
+    [DateCreate] DATETIME         DEFAULT (getdate()) NOT NULL);
+
