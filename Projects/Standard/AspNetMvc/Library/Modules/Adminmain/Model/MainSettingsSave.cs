776f1739275c3ba1def432935c14f1f17ac43546
Sellec <echelonww@gmail.com>
2020-02-18 10:00:31 +0300

    Добавлено новое соглашение по пути поиска представлений. Удалены неиспользуемые файлы. Исправлены ошибки в $form.requestJSON.before, из-за которых после изменений в common.js переставали работать формы. Частичный переход на quill после ckeditor. Добавлена конфигурация решения для сборки без проектов *.Package, т.к. это занимает много времени.

---
+++ Projects/Standard/AspNetMvc/Library/Modules/Adminmain/Model/MainSettingsSave.cs
@@ -0,0 +1,22 @@
+﻿namespace OnXap.Modules.Adminmain.Model
+{
+    using Core.Configuration;
+    using WebCoreModule;
+
+    public class MainSettingsSave
+    {
+        public MainSettingsSave() : this(new CoreConfiguration(), new WebCoreConfiguration())
+        {
+        }
+
+        public MainSettingsSave(CoreConfiguration appCoreConfiguration, WebCoreConfiguration webCoreConfiguration)
+        {
+            AppCoreConfiguration = appCoreConfiguration;
+            WebCoreConfiguration = webCoreConfiguration;
+        }
+
+        public CoreConfiguration AppCoreConfiguration { get; }
+        public WebCoreConfiguration WebCoreConfiguration { get; }
+    }
+}
+
